3992c0c9a659a8b2af02b16b6716f47d04e00b74
Laurent Grangeau <laurent.grangeau@gmail.com>
2020-02-13 11:20:46 +0100

    Add OC phrase
    Signed-off-by: Laurent Grangeau <laurent.grangeau@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 ## Do whatever you need with env vars here ...
 if [ "$CI_JOB_STAGE" == "deploy" ]
 then
-  echo "Deploy stage"
+  echo "** OpenClassrooms ** : Que la force soit avec toi !"
 fi
 
 # Hand off to the CMD
